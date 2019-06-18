c9207275742c204aeb29c210f1f8c61b24717e68
Michael <mihendricks1@gmail.com>
2019-06-18 23:19:47 +0200

    add connection string

--- src/infrastructure/db/mongodb/mongodb.conf.ts
+++ src/infrastructure/db/mongodb/mongodb.conf.ts
@@ -1,5 +1,5 @@
 export default {
   db: "mongodb://localhost/debt-angels",
   // tslint:disable-next-line:max-line-length
-  remoteUrl: ""
+  remoteUrl: "mongodb://debtangelsdb:EOZfSWfJchyT5jA98HyF7stggnD9yvXd54NOHPWpXyxPI6e0M2Lon4IB4m3EWsKgQtAvYyudioHRXpG80wzWEw==@debtangelsdb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
 };
